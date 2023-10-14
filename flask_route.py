d0602840a7355347793efe5211bb5f7e7f60dfd4
AnneDuveau <anne.duveau@ens.uvsq.fr>
2023-10-14 11:43:53 +0200

    add html

--- flask_route.py
+++ flask_route.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, world! This is a simple Flask application.'
+    return render_template('Page.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
