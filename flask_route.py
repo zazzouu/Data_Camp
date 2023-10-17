0c9b60900677d6f9e81c14437e08207a42797289
AnneDuveau <anne.duveau@ens.uvsq.fr>
2023-10-17 11:37:39 +0200

    ajout fichier css, modifications affichage interface

--- flask_route.py
+++ flask_route.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('Page.html')
+    return render_template('page.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
